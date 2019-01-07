c977bd2a775bd56e1aa76133d8388d93ca051e70
Lucy Wall <lhwall@gmail.com>
2019-01-07 20:14:13 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector("input")
+
+function retrieveEmployeeInformation(){
+return input.value
+}
+
+function addNewElementAsLi(){
+  newName = retrieveEmployeeInformation()
+  document.querySelector(".employee-list").append("<li>", newName, "</li>")
+}
+
+function insideAddnew(){
+  addNewElementAsLi(event)
+  document.querySelector("input").value = ""
+}
+
+function addNewLiOnClick(){
+  let inputType = document.querySelector("input[type='submit']")
+  inputType.addEventListener("click", insideAddnew(event)
+    )
+}
+
+function clearEmployeeListOnLinkClick(){
+  let clearButton = document.querySelector("a")
+  let list = document.querySelector(".employee-list")
+  clearButton.addEventListener("click", function(e){
+    list.innerHTML = ""
+  })
+
+}
