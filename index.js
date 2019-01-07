e89d36d7748c94f901cb3eea3d282b98636f9d73
Lucy Wall <lhwall@gmail.com>
2019-01-07 20:07:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function addNewElementAsLi(){
 
 function insideAddnew(){
   addNewElementAsLi(event)
-  document.querySelector("input").innerHTML = ""
+  document.querySelector("input").value = ""
 }
 
 function addNewLiOnClick(){
